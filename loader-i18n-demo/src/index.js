6efe407eb91a0ef8c88a83b067a7d863208d1c5c
happychen666 <2695401775@qq.com>
2023-06-06 11:00:53 +0800

    webpack编写语言转换loader

---
+++ loader-i18n-demo/src/index.js
@@ -0,0 +1,1 @@
+console.log("{{Hello}}, {{Today}} is a good day.");
---
+++ loader-i18n-demo/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index",
+  target: "node", // 编译为Node.js环境下的JS，之后直接使用Node.js执行编译完成的文件
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "i18n-loader",
+            options: {
+              // 传递选项
+              locale: "zh",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolveLoader: {
+    modules: ["./node_modules", "./loader"], // 配置loader的查找目录
+  },
+};
